5bc93161fe6711b76789701bf4b6846b35f20c1a
João Victor de Jesus Guimarães <127063578+jaumdejesus@users.noreply.github.com>
2024-04-07 23:23:42 -0300

    Estudo de Enum

---
+++ CarroEnum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace cores;
+
+enum Cores
+{
+    Branco = 1,
+    Vermelho,
+    Preto,
+    Cinza,
+    Prata,
+    Azul
+}
---
+++ Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using cores;
+
+public class Carro
+{
+	public string? Modelo { get; set; }
+	public string? Marca { get; set; }
+	public int Ano { get; set; }
+	public int Cor {  get; set; }
+
+	public Carro(string? modelo, string? marca, int ano, int cor)
+	{
+		Modelo = modelo;
+		Marca = marca;
+		Ano = ano;
+		Cor = cor;
+
+		Console.WriteLine();
+	}
+
+	public void ExibirInfo(string? modelo, string? marca, int ano, int cor)
+	{
+		Console.WriteLine($"Informações do seu carro: Modelo{modelo}, Marca:{marca}, Ano: {ano}, Cor: {cor}");
+	}
+}
